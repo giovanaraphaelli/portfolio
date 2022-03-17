1bb53124059113d7797a207f5846b54c8272e1bb
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2022-03-17 11:45:00 -0300

    add sroll animate

--- js/script.js
+++ js/script.js
@@ -8,3 +8,10 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener("click", toggleMenu);
 btnMobile.addEventListener("touchstart", toggleMenu);
+
+ScrollReveal({ reset: true });
+ScrollReveal().reveal(".sobre-texto", { delay: 400 });
+ScrollReveal().reveal(".myimg", { delay: 400 });
+ScrollReveal().reveal(".projetos-box", { delay: 400 });
+ScrollReveal().reveal(".conhecimentos-wrapper", { delay: 400 });
+ScrollReveal().reveal(".introducao", { delay: 400 });
