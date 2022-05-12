e09b858659724208319fbd63606df189347fc522
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2022-05-11 22:30:54 -0300

    arrumei bugs

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 // Menu Mobile
 
-const btnMobile = document.getElementById("btn-mobile");
+function menuMobile() {
+  const btnMobile = document.getElementById("btn-mobile");
 
-function toggleMenu(event) {
-  if (event.type === "touchstart") event.preventDefault();
-  const nav = document.getElementById("nav");
-  nav.classList.toggle("active");
+  if (btnMobile) {
+    function toggleMenu(event) {
+      if (event.type === "touchstart") event.preventDefault();
+      const nav = document.getElementById("nav");
+      nav.classList.toggle("active");
+    }
+
+    btnMobile.addEventListener("click", toggleMenu);
+    btnMobile.addEventListener("touchstart", toggleMenu);
+  }
 }
-
-btnMobile.addEventListener("click", toggleMenu);
-btnMobile.addEventListener("touchstart", toggleMenu);
+menuMobile();
 
 //ScrollRevel
-
 const sr = ScrollReveal({
   origin: "top",
   distance: "50px",
@@ -28,28 +32,35 @@ sr.reveal(".introducao", { delay: 300 });
 sr.reveal(".wrapper-btn", { delay: 300 });
 
 // Efeito Máquina de Escrever
-function typeWriter(elemento) {
-  const textoArray = elemento.innerHTML.split("");
-  elemento.innerHTML = "";
-  textoArray.forEach((letra, i) => {
-    setTimeout(() => (elemento.innerHTML += letra), 100 * i);
-  });
+function maqEscrever() {
+  function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split("");
+    elemento.innerHTML = "";
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => (elemento.innerHTML += letra), 100 * i);
+    });
+  }
+
+  const build = document.querySelector("#build");
+  if (build) {
+    typeWriter(build);
+  }
 }
 
-const build = document.querySelector("#build");
-typeWriter(build);
+maqEscrever();
 
 //Muda Descrição
-
 function mudaDescricao(nameCard, text) {
   var mudaDescricao = document.querySelector(".mudaDescricao");
 
-  document.querySelector(nameCard).addEventListener("mouseover", () => {
-    mudaDescricao.innerHTML = text;
-  });
-  document.querySelector(nameCard).addEventListener("mouseout", () => {
-    mudaDescricao.innerHTML = `*passe o cursor no card para saber mais sobre meus conhecimentos*`;
-  });
+  if (mudaDescricao) {
+    document.querySelector(nameCard).addEventListener("mouseover", () => {
+      mudaDescricao.innerHTML = text;
+    });
+    document.querySelector(nameCard).addEventListener("mouseout", () => {
+      mudaDescricao.innerHTML = `*passe o cursor no card para saber mais sobre meus conhecimentos*`;
+    });
+  }
 }
 
 mudaDescricao(
