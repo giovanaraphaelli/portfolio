3d509561506884af99634f5cd6eca5acde05690d
Giovana Raphaelli <giovana.raphaelli@cromai.com>
2025-01-15 19:25:56 -0300

    add: animation in home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,7 +11,7 @@ export function Home() {
   return (
     <div id="home" className="bg-light-background dark:bg-dark-background ">
       <div className="container mx-auto h-screen flex flex-col justify-center items-center gap-6 px-4 py-4">
-        <span className="text-lg sm:text-xl md:text-2xl text-light-text dark:text-dark-text ">
+        <span className="text-lg sm:text-xl md:text-2xl text-light-text dark:text-dark-text animate-slide-up">
           hello word!
         </span>
         <ReactMarkdown className="font-mono text-3xl md:text-6xl text-light-primary dark:text-dark-secondary">
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,91 +3,94 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: ['class'],
   theme: {
-  	extend: {
-  		fontFamily: {
-  			sans: [
-  				'Karla',
-  				'sans-serif'
-  			],
-  			mono: [
-  				'IBM Plex Mono',
-  				'mono'
-  			]
-  		},
-  		fontSize: {
-  			subtitle: [
-  				'1.5rem',
-  				{
-  					fontWeight: '500'
-  				}
-  			]
-  		},
-  		colors: {
-  			light: {
-  				background: '#DDDDF5',
-  				surface: '#C3C3E6',
-  				accent: '#cecef3',
-  				primary: '#170A33',
-  				secondary: '#4C5095',
-  				highlight: '#27272E',
-  				text: '#38383F'
-  			},
-  			dark: {
-  				background: '#18181B',
-  				surface: '#27272e',
-  				accent: '#3d3d49',
-  				primary: '#918CFF',
-  				secondary: '#C0C3FD',
-  				highlight: '#E3E3FF',
-  				text: '#B0B0D0'
-  			},
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      fontFamily: {
+        sans: ['Karla', 'sans-serif'],
+        mono: ['IBM Plex Mono', 'mono'],
+      },
+      fontSize: {
+        subtitle: [
+          '1.5rem',
+          {
+            fontWeight: '500',
+          },
+        ],
+      },
+      colors: {
+        light: {
+          background: '#DDDDF5',
+          surface: '#C3C3E6',
+          accent: '#cecef3',
+          primary: '#170A33',
+          secondary: '#4C5095',
+          highlight: '#27272E',
+          text: '#38383F',
+        },
+        dark: {
+          background: '#18181B',
+          surface: '#27272e',
+          accent: '#3d3d49',
+          primary: '#918CFF',
+          secondary: '#C0C3FD',
+          highlight: '#E3E3FF',
+          text: '#B0B0D0',
+        },
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          '1': 'hsl(var(--chart-1))',
+          '2': 'hsl(var(--chart-2))',
+          '3': 'hsl(var(--chart-3))',
+          '4': 'hsl(var(--chart-4))',
+          '5': 'hsl(var(--chart-5))',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'slide-up': {
+          '0%': { transform: 'translateY(30px)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        'slide-up': 'slide-up 2s ease-out',
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require('tailwindcss-animate')],
 };
