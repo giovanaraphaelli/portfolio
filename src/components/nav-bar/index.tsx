5945c9ddc8843b7fd210123e5d79c6beebb2c40d
Giovana Raphaelli <giovana.raphaelli@cromai.com>
2025-01-15 14:34:27 -0300

    feat: add responsive navbar

--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -1,5 +1,13 @@
+import { Drawer, DrawerContent, DrawerTrigger } from '@/components/ui/drawer';
 import { useEffect, useState } from 'react';
-import { IoMoon, IoSunny } from 'react-icons/io5';
+import { IoMenu, IoMoon, IoSunny } from 'react-icons/io5';
+
+const links = [
+  { name: 'Sobre', href: '#about' },
+  { name: 'Skills', href: '#skills' },
+  { name: 'Projetos', href: '#projects' },
+  { name: 'Contato', href: '#contact' },
+];
 
 export function Navbar() {
   const getPreferredTheme = () => {
@@ -13,6 +21,7 @@ export function Navbar() {
 
   const [theme, setTheme] = useState<'light' | 'dark'>(getPreferredTheme());
   const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     document.documentElement.classList.toggle('dark', theme === 'dark');
@@ -36,49 +45,32 @@ export function Navbar() {
           : 'bg-light-background dark:bg-dark-background'
       }`}
     >
-      <div className="container mx-auto px-2 py-2 flex justify-between items-center">
+      <div className="container mx-auto px-4 py-3 flex justify-between items-center">
+        {/* Logo */}
         <h1 className="text-sm font-bold text-light-primary dark:text-dark-secondary">
           <a href="#home">@giovanaraphaelli</a>
         </h1>
-        <div className="flex gap-5">
-          <ul className="flex gap-5 items-center font-bold text-light-secondary dark:text-dark-primary transition-colors duration-300">
-            <li>
-              <a
-                className=" hover:underline hover:text-light-primary dark:hover:text-dark-secondary"
-                href="#about"
-              >
-                Sobre
-              </a>
-            </li>
-            <li>
-              <a
-                className=" hover:underline hover:text-light-primary dark:hover:text-dark-secondary"
-                href="#skills"
-              >
-                Skills
-              </a>
-            </li>
-            <li>
-              <a
-                className=" hover:underline hover:text-light-primary dark:hover:text-dark-secondary"
-                href="#projects"
-              >
-                Projetos
-              </a>
-            </li>
-            <li>
-              <a
-                className=" hover:underline hover:text-light-primary dark:hover:text-dark-secondary"
-                href="#contact"
-              >
-                Contato
-              </a>
-            </li>
-          </ul>
+
+        <div className="flex gap-4">
+          {/* Menu Desktop */}
+          <div className="hidden md:flex gap-5 items-center">
+            <ul className="flex gap-5 font-bold text-light-secondary dark:text-dark-primary transition-colors duration-300">
+              {links.map((item, index) => (
+                <li key={index}>
+                  <a
+                    className="hover:underline hover:text-light-primary dark:hover:text-dark-secondary"
+                    href={item.href}
+                  >
+                    {item.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* Botão de troca de tema */}
           <button
             onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-            className="p-2 shadow-md rounded-full bg-light-accent dark:bg-dark-accent text-light-secondary dark:text-dark-primary hover:bg-light-secondary dark:hover:bg-dark-primary hover:text-light-accent dark:hover:text-dark-accent 
-             transition-colors duration-300"
+            className="p-2 shadow-md rounded-full bg-light-accent dark:bg-dark-accent text-light-secondary dark:text-dark-primary hover:bg-light-secondary dark:hover:bg-dark-primary hover:text-light-accent dark:hover:text-dark-accent transition-colors duration-300"
           >
             {theme === 'light' ? (
               <IoMoon className="h-6 w-6" />
@@ -86,6 +78,25 @@ export function Navbar() {
               <IoSunny className="h-6 w-6" />
             )}
           </button>
+
+          {/* Menu Mobile */}
+          <Drawer open={menuOpen} onOpenChange={setMenuOpen}>
+            <DrawerTrigger className="md:hidden p-2 shadow-md rounded-full bg-light-accent dark:bg-dark-accent text-light-secondary dark:text-dark-primary ">
+              <IoMenu className="h-6 w-6" />
+            </DrawerTrigger>
+            <DrawerContent className="bg-light-surface dark:bg-dark-surface">
+              <div className="flex justify-between items-center px-5 py-4"></div>
+              <ul className="flex flex-col gap-4 text-center font-bold text-light-secondary dark:text-dark-primary">
+                {links.map((item, index) => (
+                  <li key={index} className="py-2">
+                    <a href={item.href} onClick={() => setMenuOpen(false)}>
+                      {item.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </DrawerContent>
+          </Drawer>
         </div>
       </div>
     </nav>
