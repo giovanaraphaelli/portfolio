97a7b5c5e1650cbad752d0ab07e0c1be94cd4198
Giovana Raphaelli <giovana.raphaelli@cromai.com>
2025-01-16 11:10:36 -0300

    fix: img

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -22,7 +22,7 @@ export function About() {
         <div className="flex items-center flex-col gap-6">
           <img
             className="w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 lg:w-60 lg:h-60 rounded-full border-2 shadow-md border-light-secondary dark:border-dark-secondary"
-            src="perfil.png"
+            src="profile.jpg"
             alt="Profile"
           />
           <div ref={ref}>
