4b42cdc9354a2b6f7efa2f15206263e7a7f464f6
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2025-01-18 00:46:55 -0300

    styles: skills page

--- src/pages/skills/index.tsx
+++ src/pages/skills/index.tsx
@@ -16,16 +16,16 @@ export function SkillsGrid({ skills }: SkillsGridProps) {
 
   return (
     <Layout subtitle="<!-- skills -->" id="skills">
-      <div ref={ref} className="grid gap-6 grid-cols-3 py-8">
+      <div ref={ref} className="grid gap-4 lg:gap-6 grid-cols-3 py-8">
         {skills.map((skill, index) => (
           <div
             key={index}
-            className={`flex flex-col items-center justify-center p-6 rounded-lg bg-light-surface dark:bg-dark-surface shadow-xl opacity-0 transition-all duration-500 ${
+            className={`flex flex-col gap-1 items-center justify-center h-28 max-w-28 lg:h-36 lg:max-w-36 p-6 rounded-lg bg-light-surface dark:bg-dark-surface shadow-xl opacity-0 transition-all duration-500 ${
               inView ? 'animate-fade-in-up' : ''
             }`}
             style={{ animationDelay: `${index * 200}ms` }}
           >
-            <div className="text-4xl md:text-4xl lg:text-6xl text-light-secondary dark:text-dark-primary">
+            <div className="text-4xl lg:text-6xl text-light-secondary dark:text-dark-primary">
               {skill.icon}
             </div>
             <p className="text-sm font-medium text-center text-light-text dark:text-dark-text">
