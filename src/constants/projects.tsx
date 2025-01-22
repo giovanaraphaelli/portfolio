2114f19de9f42bb531394b9c100fc3450b6a2d05
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2025-01-22 17:10:39 -0300

    feat: add projects

--- src/constants/projects.tsx
+++ src/constants/projects.tsx
@@ -1,8 +1,16 @@
 export const projects = [
   {
+    title: 'MET Museum',
+    description:
+      'Projeto experimental desenvolvido para estudo e prática de Next.js 13+, integrando a API pública do The Metropolitan Museum of Art.',
+    img: 'met-museum.png',
+    stacks: ['Next.js 15', 'TypeScript', 'Tailwind CSS'],
+    link: 'https://github.com/giovanaraphaelli/met-museum',
+  },
+  {
     title: 'Portfolio',
     description:
-      'Meu portfólio pessoal criado com o objetivo de mostrar meus projetos ao longo da minha jornada dev.',
+      'Meu portfólio pessoal desenvolvido com o objetivo de mostrar meus projetos ao longo da minha jornada dev.',
     img: 'portfolio.png',
     stacks: ['React', 'TypeScript', 'Tailwind CSS'],
     link: 'https://github.com/giovanaraphaelli/portfolio',
@@ -10,7 +18,7 @@ export const projects = [
   {
     title: 'GitHub Blog',
     description:
-      'Este projeto consiste em uma aplicação React desenvolvida como parte do terceiro desafio da trilha de ReactJS do Ignite. A principal proposta é criar um blog pessoal consumindo a API do GitHub para exibir posts como issues de um repositório.',
+      'Aplicação React desenvolvida como parte do terceiro desafio da trilha de ReactJS do Ignite. A principal proposta é criar um blog pessoal consumindo a API do GitHub para exibir posts como issues de um repositório.',
     img: 'github-blog.png',
     stacks: ['React', 'TypeScript', 'Styled Components'],
     link: 'https://github.com/giovanaraphaelli/github-blog-rocketseat',
@@ -18,18 +26,9 @@ export const projects = [
   {
     title: 'Duna',
     description:
-      'Esta é uma aplicação sobre o livro e filme de Duna, contém descrições sobre cada personagem e pesquisa sobre terminologia.',
+      'Esta é uma aplicação sobre o livro e filme de Duna, contém descrições sobre cada personagem e busca sobre as principais terminologias do mundo de Frank Herbert.',
     img: 'duna.png',
-    stacks: ['Next js', 'JavaScript'],
+    stacks: ['Next.js 12', 'JavaScript'],
     link: 'https://github.com/giovanaraphaelli/duna',
-  },
-
-  {
-    title: 'fav-lol-champions',
-    description:
-      'Página Criada com o intuito de mostrar meus campeões favoritos de League of Legends e suas habilidades.',
-    img: 'fav-lol-champions.png',
-    stacks: ['HTML', 'CSS', 'JavaScript'],
-    link: 'https://github.com/giovanaraphaelli/fav-lol-champions',
   },
 ];
