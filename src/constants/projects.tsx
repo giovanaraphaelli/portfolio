b2609806ec36282735591b9f18617a1d304434e7
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2025-01-29 20:25:29 -0300

    feat: add new project "Chat App"

--- src/constants/projects.tsx
+++ src/constants/projects.tsx
@@ -1,5 +1,12 @@
 export const projects = [
   {
+    title: 'Chat App',
+    description: 'Aplicação de chat em tempo real com autenticação via Github.',
+    img: 'chat-app.png',
+    stacks: ['Next.js 15', 'TypeScript', 'Supabase', 'Zustand', 'Tailwind CSS'],
+    link: 'https://github.com/giovanaraphaelli/chat-app',
+  },
+  {
     title: 'MET Museum',
     description:
       'Projeto experimental desenvolvido para estudo e prática de Next.js 13+, integrando a API pública do The Metropolitan Museum of Art.',
@@ -8,20 +15,20 @@ export const projects = [
     link: 'https://github.com/giovanaraphaelli/met-museum',
   },
   {
-    title: 'Portfolio',
-    description:
-      'Meu portfólio pessoal desenvolvido com o objetivo de mostrar meus projetos ao longo da minha jornada dev.',
-    img: 'portfolio.png',
-    stacks: ['React', 'TypeScript', 'Tailwind CSS'],
-    link: 'https://github.com/giovanaraphaelli/portfolio',
-  },
-  {
     title: 'GitHub Blog',
     description:
       'Aplicação React desenvolvida como parte do terceiro desafio da trilha de ReactJS do Ignite. A principal proposta é criar um blog pessoal consumindo a API do GitHub para exibir posts como issues de um repositório.',
     img: 'github-blog.png',
     stacks: ['React', 'TypeScript', 'Styled Components'],
     link: 'https://github.com/giovanaraphaelli/github-blog-rocketseat',
+  },
+  {
+    title: 'Portfolio',
+    description:
+      'Meu portfólio pessoal desenvolvido com o objetivo de mostrar meus projetos ao longo da minha jornada dev.',
+    img: 'portfolio.png',
+    stacks: ['React', 'TypeScript', 'Tailwind CSS'],
+    link: 'https://github.com/giovanaraphaelli/portfolio',
   },
   {
     title: 'Duna',
